b3dc15cea0badf1d0cb10e2aa7fbdf3385777168
Sara Gamal <sgglnashed@gmail.com>
2024-11-29 18:10:59 +0200

    finish req 4

--- Problem Set 2/cryptarithmetic.py
+++ Problem Set 2/cryptarithmetic.py
@@ -42,6 +42,7 @@ class CryptArithmeticProblem(Problem):
         problem = CryptArithmeticProblem()
         problem.LHS = (LHS0, LHS1)
         problem.RHS = RHS
+        # Reverse the strings to make it easier to work with
         LHS0=LHS0[::-1]
         RHS=RHS[::-1]
         LHS1=LHS1[::-1]
@@ -57,10 +58,13 @@ class CryptArithmeticProblem(Problem):
         #commonly used ranges for domains 
         digit_set = set(range(10))
         non_zero_digit_set = set(range(1, 10))
+        #set for carry 
         carry_set = set(range(2))
-        medium_set = set(range(100))-{x * 11 for x in range(10)}
+      #set for carry + digit
         small_sum_set = set(range(20))
-
+        #set for carry + 2 different digits
+        medium_set = set(range(100))-{x * 11 for x in range(10)}
+        #set for carry + 3 different digits
         large_sum_set = set(range(200))-{x * 11 for x in range(10)}-set(range(111, 200, 11))
         
         # Add "all-different" constraints
@@ -70,6 +74,7 @@ class CryptArithmeticProblem(Problem):
     
         # Assign domains to variables            
         for variable in problem.variables:
+            # If the variable is the first letter of a word, it can't be 0
             if variable in (LHS0[-1], LHS1[-1], RHS[-1]):
                 problem.domains[variable] = non_zero_digit_set
             else:
@@ -80,65 +85,73 @@ class CryptArithmeticProblem(Problem):
         min_len = min(len(LHS0), len(LHS1))
         max_lhs = LHS1 if len(LHS1) > len(LHS0) else LHS0
 
+        # Add variables and domains for carry 
+        problem.variables.extend([f"C{i}" for i in range(max_len)])
+        problem.domains.update({f"C{i}": carry_set for i in range(max_len)})
 
-        problem.constraints.append(BinaryConstraint((f"{LHS1[0]}{LHS0[0]}", f"{RHS[0]}C{0}"), lambda a, b: (a % 10 + a // 10) == b % 10 + 10 * (b // 10)  ))
-        problem.variables.append(f"{LHS1[0]}{LHS0[0]}")
-        problem.domains[f"{LHS1[0]}{LHS0[0]}"] = medium_set if LHS0[0] != LHS1[0] else {x * 11 for x in range(10)}
+
+        # Add variables and domains for sum(RHS[i] + carry[i])
+        problem.variables.extend([f"{RHS[i]}C{i}" for i in range(max_len)])
+        problem.domains.update({f"{RHS[i]}C{i}": small_sum_set for i in range(max_len)})
+        
+
+        # Add variables and domains for sum(LHS1[0] + LHS0[0])
+        problem.variables.extend([f"{LHS1[0]}{LHS0[0]}"])
+        problem.domains.update({f"{LHS1[0]}{LHS0[0]}": medium_set if LHS0[0] != LHS1[0] else {x * 11 for x in range(10)} })
+        # Add variables and domains for sum(LHS1[i] + LHS0[i] + carry[i])
+        problem.variables.extend([f"{LHS1[i+1]}{LHS0[i+1]}C{i}" for i in range(min_len-1)])
+        problem.domains.update({f"{LHS1[i+1]}{LHS0[i+1]}C{i}": large_sum_set  if LHS0[i + 1] != LHS1[i + 1]   else {x * 11 for x in range(10)} | set(range(111, 200, 11))  for i in range(min_len-1)})
+        # Add variables and domains for sum(max_lhs[i] + carry[i])  
+        #where max_lhs is the longer of the two LHS strings(if they are of different lengths)
+        problem.variables.extend([ f"{max_lhs[i+1]}C{i}" for i in range(min_len-1,len(max_lhs) - 1) ])
+        problem.domains.update({f"{max_lhs[i+1]}C{i}": small_sum_set for i in range(min_len-1,len(max_lhs) - 1)})
+        #add constraints for the first digit of the sum of the two LHS strings
         problem.constraints.extend([
         BinaryConstraint((f"{LHS1[0]}{LHS0[0]}", LHS1[0]), lambda a, b: a // 10 == b),
         BinaryConstraint((f"{LHS1[0]}{LHS0[0]}", LHS0[0]), lambda a, b: a % 10 == b)])                    
-
+        #add constraints for the first digit of the sum of the two LHS strings and the RHS
+        problem.constraints.append(BinaryConstraint((f"{LHS1[0]}{LHS0[0]}", f"{RHS[0]}C{0}"), lambda a, b: (a % 10 + a // 10) == b % 10 + 10 * (b // 10)  ))
+      
         for i in range(max_len):
            
-            # Handle carries
             carry_var = f"C{i}"
-            problem.variables.append(carry_var)
-            problem.domains[carry_var] = carry_set
-
             sum_var = f"{RHS[i]}C{i}"
-            problem.variables.append(sum_var)
-            problem.domains[sum_var] = small_sum_set
+          #add constraints for the RHS and the carry with the sum of the strings
             problem.constraints.extend([
               BinaryConstraint((sum_var, carry_var), lambda a, b: a // 10 == b),
               BinaryConstraint((sum_var, RHS[i]), lambda a, b: a % 10 == b)])
-
+            #when the strings both havn't ended
             if i < min_len - 1:
-                combined_var = f"{LHS1[i+1]}{LHS0[i+1]}"
-                problem.variables.append(combined_var)
-                problem.domains[combined_var] = medium_set if LHS0[i+1] != LHS1[i+1] else {x * 11 for x in range(10)}
-                problem.constraints.extend([
-               BinaryConstraint((combined_var, LHS1[i+1]), lambda a, b: a // 10 == b),
-               BinaryConstraint((combined_var, LHS0[i+1]), lambda a, b: a % 10 == b)])                    
-
-                next_combined_var = f"{LHS1[i+1]}{LHS0[i+1]}C{i}"
-                problem.variables.append(next_combined_var)
-                problem.domains[next_combined_var] = (
-                    large_sum_set 
-                    if LHS0[i + 1] != LHS1[i + 1] 
-                    else {x * 11 for x in range(10)} | set(range(111, 200, 11))
-                )
+                combined_var = f"{LHS1[i+1]}{LHS0[i+1]}C{i}"
 
                 problem.constraints.extend([
-                    BinaryConstraint((next_combined_var, f"{LHS1[i+1]}{LHS0[i+1]}"), lambda a, b: a % 100 == b),
-                    BinaryConstraint((next_combined_var, carry_var), lambda a, b: a // 100 == b),
-                    BinaryConstraint((next_combined_var, f"{RHS[i+1]}C{i+1}"), lambda a, b: (
+                    BinaryConstraint((combined_var, carry_var), lambda a, b: a // 100 == b),  #constraint for carry with combined variable
+                    BinaryConstraint((combined_var, f"{RHS[i+1]}C{i+1}"), lambda a, b: (
                         a // 100 + (a % 100) // 10 + (a % 100) % 10 == b % 10 + 10 * (b // 10)
-                    ))
+                    )),  #constraint for RHS with combined variable (to make sure the sum is correct)
+               BinaryConstraint((combined_var, f"{LHS1[i+1]}"), lambda a, b: (a % 100)%10 ==b), #constraint for LHS1 with combined variable
+               BinaryConstraint((combined_var, f"{LHS0[i+1]}"), lambda a, b: (a % 100)//10 ==b) #constraint for LHS0 with combined variable
                 ])
+            #when a string has ended but the longer string hasn't
             elif i < len(max_lhs) - 1:
-                max_var = f"{max_lhs[i+1]}C{i}"
-                problem.variables.append(max_var)
-                problem.domains[max_var] = small_sum_set
+                combined_var = f"{max_lhs[i+1]}C{i}"
+                
                 problem.constraints.extend([
-              BinaryConstraint((max_var, max_lhs[i+1]), lambda a, b: a % 10 == b),
-              BinaryConstraint((max_var, carry_var), lambda a, b: a // 10 == b),
-              BinaryConstraint((max_var, f"{RHS[i+1]}C{i+1}"), lambda a, b: (
-                    a // 10 + a % 10 == b % 10 + 10 * (b // 10)
-                ))])
+             BinaryConstraint((combined_var, max_lhs[i+1]), lambda a, b: a % 10 == b), #constraint for the longer string with the combined variable
+              BinaryConstraint((combined_var, carry_var), lambda a, b: a // 10 == b),  #constraint for carry with combined variable
+              BinaryConstraint((combined_var, f"{RHS[i+1]}C{i+1}"), lambda a, b: (
+                    a // 10 + a % 10 == b % 10 + 10 * (b // 10)  )) #constraint for RHS with combined variable (to make sure the sum is correct)
+                    ])
 
         if len(RHS) > max_len :
-            problem.constraints.append(BinaryConstraint((RHS[-1], f"C{max_len-1}"), lambda a, b: a == b))
-       
+            #update the domain for the last carry if the RHS is longer than the sum of the two LHS strings
+            #because last digit can only be 1 and last carry=last digit
+            problem.domains[RHS[-1]] = set([1])
+            problem.domains[f"C{max_len-1}"] = set([1])
+        else:
+            #update the domain for the last carry if the RHS is not longer than the sum of the two LHS strings
+            #last carry must be 0
+            problem.domains[f"C{max_len-1}"] = set([0])
         return problem
 
 
@@ -147,3 +160,6 @@ class CryptArithmeticProblem(Problem):
     def from_file(path: str) -> "CryptArithmeticProblem":
         with open(path, 'r') as f:
             return CryptArithmeticProblem.from_text(f.read())
+        
+
+
